3edd0b87dd3c5a4e23c5cd680a0f567ecb36dc0d
iacostac <infoivan1@gmail.com>
2020-12-03 21:10:20 -0300

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { TarjetaComponent } from './components/tarjeta/tarjeta.component';
 import { TarjeteroComponent } from './components/tarjetero/tarjetero.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     TarjetaComponent,
-    TarjeteroComponent
+    TarjeteroComponent,
+    NosotrosComponent
   ],
   imports: [
     BrowserModule,
